3c4681ebf62539d7b68cb51d6509947d1ae46f59
Martin <martin@Martins-MacBook-Pro-2.local>
2016-04-07 08:30:28 +0200

    clean up

--- exercise02/src/at/archkb/server/business/user/UserBusiness.java
+++
@@ -1,9 +0,0 @@
-package at.archkb.server.business.user;
-
-import org.springframework.security.provisioning.UserDetailsManager;
-
-import at.archkb.server.entity.User;
-
-public interface UserBusiness extends UserDetailsManager {
-	User getUser(String username);
-}
